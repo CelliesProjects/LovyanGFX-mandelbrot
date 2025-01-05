49a74baed392966d40004ba0c2d22a968a69b00e
Cellie <CelliesProjects@users.noreply.github.com>
2025-01-05 22:47:24 +0100

    WIP

--- src/cartesianwindow.h
+++ src/cartesianwindow.h
@@ -1,20 +1,26 @@
 #ifndef CARTESIANWINDOW_H
 #define CARTESIANWINDOW_H
 
-#define LGFX_M5STACK
 #include <LovyanGFX.hpp>
-#include <LGFX_AUTODETECT.hpp>  // クラス"LGFX"を用意します
+#include <LGFX_AUTODETECT.hpp> // クラス"LGFX"を用意します
 
-extern LGFX display; 
+extern LGFX display;
 
-class CartesianWindow {
+class CartesianWindow
+{
 private:
-    LGFX& _display;
+    LGFX &_display;
     float _originX, _originY;
     float _scaleX, _scaleY;
 
 public:
-    CartesianWindow(LGFX& display);
+    CartesianWindow(LGFX &display)
+        : _display(display), _originX(0), _originY(0), _scaleX(1), _scaleY(1) {}
+
+    LGFX& getDisplay() {
+        return _display;
+    }
+
     void setWindow(float x, float y, float width, float height);
     void getPixelCoordinates(float x, float y, int16_t &px, int16_t &py) const;
     void drawCartesianPixel(float x, float y, uint16_t color);
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,86 @@
-#define LGFX_M5STACK
 #include <LovyanGFX.hpp>
-#include <LGFX_AUTODETECT.hpp>  // クラス"LGFX"を用意します
+// #include <LGFX_AUTODETECT.hpp>  // クラス"LGFX"を用意します
 #include "cartesianwindow.h"
 
-
 // Create an instance of the LovyanGFX display
-LGFX display; 
+LGFX display;
 
 // Create the CartesianWindow and link it to the display
 CartesianWindow cartesianWindow(display);
 
-void setup() {
-    display.init();                     // Initialize the M5Stack Fire display
-    display.setRotation(1);             // Set display rotation (landscape)
+void drawMandelbrot(CartesianWindow &cartesianWindow, float xMin, float xMax, float yMin, float yMax, int maxIterations)
+{
+    // Get the display reference from the CartesianWindow
+    LGFX &display = cartesianWindow.getDisplay();
 
-    cartesianWindow.setWindow(-2.0, -1.5, 3.0, 3.0); // Set a 3x3 Cartesian space
+    // Iterate over every pixel in the display
+    for (int i = 0; i < display.width(); i++) {
+        for (int j = 0; j < display.height(); j++) {
+            // Map pixel coordinates to Cartesian coordinates
+            float x0 = xMin + (i * (xMax - xMin) / display.width());
+            float y0 = yMin + (j * (yMax - yMin) / display.height());
 
-    // Annotate the Cartesian space
-    int16_t px, py;
-    cartesianWindow.getPixelCoordinates(-1.0, 0.5, px, py);
-    display.setTextColor(TFT_WHITE);
-    display.drawString("Point (-1.0, 0.5)", px, py);
+            float x = 0.0, y = 0.0;
+            int iteration = 0;
 
-    // Draw something in the Cartesian space
-    cartesianWindow.drawCartesianPixel(-1.0, 0.5, TFT_WHITE);
+            // Mandelbrot iteration to calculate escape time
+            while (x * x + y * y <= 4.0 && iteration < maxIterations) {
+                float xTemp = x * x - y * y + x0;
+                y = 2.0 * x * y + y0;
+                x = xTemp;
+                iteration++;
+            }
+
+            // Color the point based on the number of iterations it took to escape
+            uint16_t color;
+
+            // Points inside the Mandelbrot set (iteration == maxIterations) remain black
+            if (iteration == maxIterations) {
+                color = TFT_BLACK;  // Black color for points inside the set
+            }
+            else {
+                // Color based on how fast the point escapes
+                int escapeColor = (iteration * 255 / maxIterations); // Scale iteration to color range
+                color = display.color565(escapeColor, 0, 255 - escapeColor); // Gradient from blue to red
+            }
+
+            // Draw the point using the Cartesian coordinate system
+            cartesianWindow.drawCartesianPixel(x0, y0, color);
+        }
+    }
 }
 
-void loop() {
+void setup()
+{
+    display.init();         // Initialize the M5Stack Fire display
+    display.setRotation(1); // Set display rotation (landscape)
+/*
+    cartesianWindow.setWindow(-1.0, -1.0, 2.0, 2.0); // Set a 3x3 Cartesian space
+
+    int16_t px, py;
+    cartesianWindow.getPixelCoordinates(0, 0, px, py);
+    display.setTextColor(TFT_WHITE);
+    display.drawCircle(px, py, 5, display.color565(255, 0, 0));
+    display.drawString("Point (0, 0)", px, py);
+
+    // Draw something in the Cartesian space
+    for (float x = -1.5; x <= 1.5; x = x + 0.01f)
+    {
+        cartesianWindow.drawCartesianPixel(x, x*x, display.color565(255, 255, 255));
+    }
+
+    delay(1000);
+*/
+    // Set up the view window for the Mandelbrot set
+    float xMin = -1.5, xMax = 1.5, yMin = -1.5, yMax = 1.5;
+    int maxIterations = 250;
+
+    // Draw the Mandelbrot set
+    cartesianWindow.setWindow(xMin, yMin, xMax - xMin, yMax - yMin); // Define the Cartesian space
+    drawMandelbrot(cartesianWindow, xMin, xMax, yMin, yMax, maxIterations); // Draw the fractal
+}
+
+void loop()
+{
     // Additional drawing or interaction logic
 }
